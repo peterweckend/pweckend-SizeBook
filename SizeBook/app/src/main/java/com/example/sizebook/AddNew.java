7ade8168c1ad255d6002203e50bbd531fffb3046
Peter Weckend <PeterWeckend@Peters-MBP.hitronhub.home>
2017-01-31 22:40:05 -0700

    Add New/Submit buttons now direct to activities

--- SizeBook/app/src/main/java/com/example/sizebook/AddNew.java
+++ SizeBook/app/src/main/java/com/example/sizebook/AddNew.java
@@ -1,7 +1,11 @@
 package com.example.sizebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AddNew extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class AddNew extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
+
+
+
+        Button submit_addnew = (Button) findViewById(R.id.submit_button_addnew);
+
+        submit_addnew.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "submit", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AddNew.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
+
 }
--- SizeBook/app/src/main/java/com/example/sizebook/MainActivity.java
+++ SizeBook/app/src/main/java/com/example/sizebook/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.sizebook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button addnew_main = (Button) findViewById(R.id.addnew_button_main);
+
+        addnew_main.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "add new", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, AddNew.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+
+
+
 }
